e60c02c586dbd5d592a0249558e29dc1de26932c
lordkaus <lmatostmelo@gmail.com>
2026-03-24 09:21:16 -0300

    Fix PRODUCT_NAME to match lunch command

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,5 @@
 PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_p80.mk
 
-COMMON_LUNCH_CHOICES := twrp_p80-eng
+COMMON_LUNCH_CHOICES := \
+    twrp_p80-eng \
+    twrp_p80-userdebug
--- twrp_p80.mk
+++ twrp_p80.mk
@@ -1,9 +1,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, vendor/otrp/config/common.mk)
 
-PRODUCT_NAME := orange_p80
+# Se estiver usando OrangeFox 12.1, o caminho correto da config costuma ser este:
+$(call inherit-product, vendor/recovery/config/common.mk)
+
 PRODUCT_DEVICE := p80
+PRODUCT_NAME := twrp_p80
 PRODUCT_BRAND := Cubot
 PRODUCT_MODEL := P80
 PRODUCT_MANUFACTURER := cubot
+
+PRODUCT_GMS_CLIENTID_BASE := android-cubot
